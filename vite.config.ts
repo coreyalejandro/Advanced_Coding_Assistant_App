687841f48a158ac18b593ece07bb0199e3a69c18
Corey Alejandro <corey@coreyalejandro.com>
2025-10-28 23:20:43 -0500

    Fix __dirname issue in vite config for ES modules

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import { resolve } from 'path';
+  import { fileURLToPath, URL } from 'node:url';
 
   export default defineConfig({
     plugins: [react()],
     resolve: {
       alias: {
-        '@': resolve(__dirname, './src'),
+        '@': resolve(fileURLToPath(new URL('./src', import.meta.url))),
       },
     },
     build: {
